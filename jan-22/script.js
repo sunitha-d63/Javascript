70af2191e0ac6be5dd1abb9d7222966fabda2744
sunitha-d63 <sunithagopal06@gmail.com>
2025-01-22 19:00:14 +0530

    Add files via upload

---
+++ jan-22/script.js
@@ -0,0 +1,100 @@
+
+// Print numbers from 1 to 10
+
+for(a=1;a<=10;a++){
+    console.log(a)
+}
+
+
+// Print even numbers from 1 to 20
+
+console.log(`task-2`);
+
+for(x=1;x<=20;x++)
+{
+    x++;
+    console.log(x);
+}
+
+// Print odd numbers from 1 to 20
+
+console.log(`task-3`);
+let y=1;
+
+for (y=1;y<=20;y++)
+{
+    if(y%2)
+        {
+            console.log(y);
+        }
+}
+
+
+// Sum of numbers from 1 to 10
+
+console.log(`task-4`);
+ let c=0;
+ let sum=10;
+
+for(i=0;i<=sum;i++) 
+ {
+    // console.log(i);
+    c=c+i;
+   
+ }
+console.log(c);
+
+// Factorial of a number (e.g., 5!)
+console.log(`task-5`);
+
+let num=1;
+let Factorial=5;
+for(f=1;f<=Factorial;f++)
+{
+    num=num*f;
+
+}
+console.log(num);
+
+// Print a multiplication table of 5
+
+console.log(`task-6`);
+number=5;
+
+for (let m = 1; m <= 10; m++) 
+    {
+       result = number * m;
+       console.log(`${number}*${m}=${result}`);
+    }
+
+
+// Print numbers from 10 to 1
+
+console.log(`task-8`);
+for(p=10;p>=1;p--){
+    console.log(p)
+}
+
+
+// while loop
+
+// Print numbers from 1 to 10
+console.log(`task-11`);
+let w=0;
+while(w<10)
+    {
+        w++;
+        console.log(w);
+    }
+
+
+// Print even numbers from 1 to 20
+console.log(`task-12`);
+let e=1;
+
+while(e<=20)
+{
+    e++;
+    console.log(e);
+}
+
