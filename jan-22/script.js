cea0c7662dc0e6fff7d4f308d5d4342ea6e90572
sunitha-d63 <sunithagopal06@gmail.com>
2025-01-23 10:32:22 +0530

    Add files via upload

---
+++ jan-22/script.js
@@ -0,0 +1,210 @@
+
+// Print numbers from 1 to 10
+
+for(a=1;a<=10;a++){
+    console.log(a)
+}
+
+
+// Print even numbers from 1 to 20
+
+console.log(`task-2`);
+
+for(x=1;x<=20;x++)
+{
+    x++;
+    console.log(x);
+}
+
+console.log(`Step -2`);
+
+let i 
+for(i=1;i<=20;i++){
+if(i%2==0){
+console.log(i)
+ }
+}
+
+// Print odd numbers from 1 to 20
+
+console.log(`task-3`);
+let y=1
+
+for (y=1;y<=20;y++)
+{
+    if(y%2)
+        {
+            console.log(y);
+        }
+}
+
+
+// Sum of numbers from 1 to 10
+
+console.log(`task-4`);
+ let c=0
+ let sum=10
+
+for(i=0;i<=sum;i++) 
+ {
+    // console.log(i);
+    c=c+i;
+   
+ }
+console.log(c);
+
+// Factorial of a number (e.g., 5!)
+console.log(`task-5`);
+
+let num=1
+let Factorial=5
+for(f=1;f<=Factorial;f++)
+{
+    num=num*f;
+
+}
+console.log(num);
+
+// Print a multiplication table of 5
+
+console.log(`task-6`);
+number=5;
+
+for (let m = 1; m <= 10; m++) 
+    {
+       result = number * m;
+       console.log(`${number}*${m}=${result}`);
+    }
+
+
+// Print numbers from 10 to 1
+
+console.log(`task-7`);
+for(p=10;p>=1;p--){
+    console.log(p)
+}
+
+
+// while loop
+
+// Print numbers from 1 to 10
+console.log(`task-8`);
+let w=0;
+while(w<10)
+    {
+        w++;
+        console.log(w);
+    }
+
+
+// Print even numbers from 1 to 20
+console.log(`task-9`);
+let e=1
+
+while(e<=20)
+{
+    if(e%2==0){
+        console.log(e);
+    }
+    e++
+     
+}
+
+// Print odd numbers from 1 to 20
+
+console.log(`task-10`);
+
+let o=1
+while(o<=20)
+{
+    if(o%2)
+    {
+        console.log(o);
+    }
+    o++
+}
+
+
+// Sum of numbers from 1 to 10
+
+console.log(`task-11`);
+
+let N = 1
+let T=0
+
+while (N<=10){
+  
+  if(N%2==0){
+    T= T + N
+  }
+
+  N++
+}
+console.log(T)
+
+
+
+
+// Print a multiplication table of 5
+
+console.log(`task-12`);
+
+num=5
+let m=1
+while(m<=10)
+{
+ console.log(`${num}*${m}=${num*m}`);
+ m++;
+
+}
+ 
+
+// Print numbers from 10 to 1
+
+console.log(`task-13`);
+
+p=11
+while(p>=2)
+{
+    p--
+    console.log(p)
+   
+}
+
+
+// Factorial of a number (e.g., 5!)
+
+console.log(`task-14`);
+
+num=1
+Factorial=5
+f=1
+while(f<=Factorial){
+   
+    num=num*f;
+    f++
+}
+console.log(num);
+
+
+// Print numbers from 1 to 100, but skip multiples of 3
+
+console.log(`task-15`);
+for(number = 1; number <= 100; number ++)
+    if(number % 3 != 0){
+        console.log(number);
+    }
+
+ console.log(`task-16`);
+
+ number = 1
+ while(number <= 100)
+ {
+    
+    if(number % 3 != 0){
+        console.log(number);
+    }
+    number ++
+ }
+
+
