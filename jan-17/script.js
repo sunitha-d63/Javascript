9c743ddc36254e7970a7a9595b83dc66f7dfcd09
sunitha-d63 <sunithagopal06@gmail.com>
2025-01-24 08:23:45 +0530

    Add files via upload

---
+++ jan-17/script.js
@@ -0,0 +1,104 @@
+/* add */
+
+let a=20;
+let b=30;
+let c=a+b;
+console.log(`addition,${c}`);
+
+// Subtract 
+
+let d=5;
+let e=10;
+let f=a-b;
+console.log(f);
+console.log(`Subtract ${f}`);
+
+// Multiply 
+
+let x=20;
+let y=30;
+let product=x*y;
+console.log(product);
+
+
+// Divide 
+let g=6;
+let h=9;
+let quotient=g/h;
+console.log(quotient);
+
+
+// Modulus
+let A=7;
+let B=3;
+let Remainder=A%B;
+console.log(Remainder);
+
+
+// Exponentiation
+let C=3;
+let D=4;
+let Exponentiation=3 ** 4
+console.log(Exponentiation);
+
+
+// Assignment
+let X=10;
+console.log(X);
+
+
+// Add and Assign
+
+let Z = 2; 
+let Y = 5; 
+console.log(Z += Y); 
+
+
+// Subtract and Assign
+
+let number = 9; 
+number -= 5; 
+console.log(number)
+
+// Multiply and Assign
+let num = 5; 
+num *= 2; 
+console.log(num);
+
+// Divide and Assign
+
+let O = 18; 
+O /= 3; 
+console.log(O); 
+// Remainder and Assign
+
+let Rem = 3;
+ 
+console.log((Rem %= 2));
+
+// Exponentiation and Assign
+
+let Exp = 6; 
+console.log(Exp **= 2);
+
+
+// Increment Operator
+let Inc = 10;
+console.log(Inc++);
+console.log(Inc);
+
+
+let pre = 10;
+console.log(++pre);
+console.log(pre);
+
+// Decrement Operator
+
+let Dec = 10; 
+console.log(Dec--); 
+console.log(Dec);
+
+
+let PRE = 10; 
+console.log(--PRE); 
+console.log(PRE);
---
+++ jan-18/script.js
@@ -0,0 +1,56 @@
+// Equality Check
+let x=5;
+console.log(x=='5');
+console.log(x==='5');
+
+// Inequality Check
+let a=10;
+console.log(a!=15);
+
+// Greater Than Comparison
+let X=25;
+let Y=20;
+console.log(X>Y);
+
+// Less Than Comparison
+let A=8;
+let B=12;
+console.log(A<B);
+
+// Greater Than or Equal Comparison
+let b=30;
+let c=30;
+console.log(b>=c)
+
+// Less Than or Equal Comparison
+let d=7;
+let e=10;
+console.log(d<=e);
+
+// AND Operator
+console.log(10>5&&20>15);
+
+// OR Operator
+console.log(5>10 || 15>10);
+
+// NOT Operator
+console.log (!(10>20));
+
+// Combined AND and OR
+console.log ((10>5 && 15>20)||(5<10));
+
+// Between Range Check
+console.log(15>10 && 12<20);
+
+// Outside Range Check--------
+console.log(25>10 && 20<25);
+
+// Equality with NOT
+console.log(5!=="5");
+
+// Multiple Comparisons
+console.log(10>5&&20>15&&30<40);
+
+// Opposite Conditions
+console.log(10>20 && 5<15);
+console.log(!(10>20 && 5<15));
