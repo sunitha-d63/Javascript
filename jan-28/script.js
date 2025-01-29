390339c639331b5631c538359146e01bbf3d2682
sunitha-d63 <sunithagopal06@gmail.com>
2025-01-29 09:31:31 +0530

    Add files via upload

---
+++ jan-28/script.js
@@ -0,0 +1,77 @@
+// Function to Add Two Numbers
+console.log(`task-1`);
+
+function addNumbers(a,b){
+return a+b;
+}
+console.log(addNumbers(5,3));
+
+// Function with Default Parameters
+console.log(`task-2`);
+function greet(name="Guest"){
+console.log("Hello,"+ name +"!")
+}
+greet();
+
+// Arrow Function to Find Square of a Number
+console.log(`task-3`);
+const square = (numb) => {
+    return numb*numb;
+}
+console.log(`${square(2)}`);
+
+// Function to Convert Celsius to Fahrenheit
+console.log(`task-4`);
+function celciusToFahrenheit(celcius) {
+    return celcius*9/5+32;
+}
+console.log(`${celciusToFahrenheit(20)}`);
+
+// Largest of Three Numbers
+console.log(`task-5`);
+function findLargest(a,b,c){
+    if(a>=b && a<=c){
+        return a;
+    }
+    else  if(b>=a && b<=c){
+        return b;
+    }
+    else{
+        return c;
+    }
+}
+console.log(findLargest(5,10,20))
+
+
+// Calculate the Area of a Circle
+// Formula: Area = π * radius^2
+// π  is a constant approximately equal to 3.14159
+
+console.log(`task-6_Area of circle`);
+function calculateCircleArea(radius){
+    return 3.14*radius^2;
+}
+console.log(`${calculateCircleArea(6)}`);
+
+
+// Function Using Callback
+console.log(`task-7`);
+
+let a=5
+let b=3
+
+function performOperation(first,second){
+    first();
+    second();
+    
+}
+function add(){
+    let result=a+b;
+    console.log(`${result}`);
+    
+}
+function sub(){
+    let result=a-b;
+    console.log(`${result}`);
+}
+performOperation(add,sub)
