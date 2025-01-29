fd7995af9e028ee04d856f8fdd28a255de8352fe
sunitha-d63 <sunithagopal06@gmail.com>
2025-01-29 21:34:26 +0530

    Add files via upload

---
+++ jan-29/script.js
@@ -0,0 +1,45 @@
+console.log(`task-1`)
+console.log(true+undefined);
+console.log(typeof(false+undefined),false+undefined);
+console.log(false-undefined);
+
+
+console.log(`task-2`)
+const result=Number("hi")
+console.log(result);
+
+console.log(`task-3`)
+console.log(Number(undefined));
+
+console.log(`task-4`)
+console.log(Number(''));
+
+console.log(`task-5`)
+value = "3" + null; 
+console.log(typeof(value));
+console.log(value);
+
+console.log(`task-6`)
+const str=Boolean(" ")
+console.log(str);
+
+console.log(`task-7`)
+console.log(('10'-10)+7+7);
+
+console.log(`task-8`)
+console.log(+"20"-(10)+3+"7");
+
+console.log(`task-9`)
+console.log("5"+ +true +"greater than"+ -false);
+console.log("5"+ -true +"value"+ -false);
+
+
+console.log(`task-10`)
+console.log(+"2"*null-true);
+
+console.log(`task-11`)
+var x = ["twenty"];
+console.log(Number(x));
+
+var y = [20];
+console.log(String(y));
