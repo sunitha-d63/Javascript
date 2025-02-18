5ccbba87070f9709f7f0ad22150bf16cab31fa12
sunitha-d63 <sunithagopal06@gmail.com>
2025-02-18 14:52:16 +0530

    Add files via upload

---
+++ feb-17-nav/assets/script.js
@@ -0,0 +1,21 @@
+
+function toggleMenu() {
+    const navlist = document.querySelector("#list");
+    console.log(navlist);
+
+
+    if (navlist.style.display == "none") {
+        navlist.style.display = "block"
+    }
+    else {
+        navlist.style.display = "none"
+    }
+
+
+}
+
+
+
+
+
+
